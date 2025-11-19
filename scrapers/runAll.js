443ec1b2f5e134f06fa89657374c81cc8c95320e
devbyty <73771147+devbyty@users.noreply.github.com>
2025-11-18 23:56:24 -0500

    Update runAll.js

--- scrapers/runAll.js
+++ scrapers/runAll.js
@@ -1,34 +1,14 @@
 const fs = require("fs");
 
-const scrapeRhinegeist = require("./rhinegeist");
-const scrapeCityBeat = require("./citybeat");
-const scrapeFindlay = require("./findlay");
-const scrapeParks = require("./parks");
-const scrapeVisitCincy = require("./visitcincy");
 const scrapeEventbrite = require("./eventbrite");
 
 async function runAll() {
   console.log("Running scrapers...");
 
-  const rg = await scrapeRhinegeist();
-  console.log("Rhinegeist:", rg.length);
-
-  const cb = await scrapeCityBeat();
-  console.log("CityBeat:", cb.length);
-
-  const fm = await scrapeFindlay();
-  console.log("Findlay:", fm.length);
-
-  const pk = await scrapeParks();
-  console.log("Parks:", pk.length);
-
-  const vc = await scrapeVisitCincy();
-  console.log("VisitCincy:", vc.length);
-
   const eb = await scrapeEventbrite();
   console.log("Eventbrite:", eb.length);
 
-  const allEvents = [...rg, ...cb, ...fm, ...pk, ...vc, ...eb];
+  const allEvents = [...eb];
 
   console.log("TOTAL EVENTS:", allEvents.length);
 
